4e2f76bba213eec68163d9080979754587640213
Leon Sandøy <leon.sandoy@conmodo.com>
2021-04-26 18:22:28 +0200

    Fix broken health bar gradient.

--- Healthbar.gd
+++ Healthbar.gd
@@ -5,8 +5,11 @@ export (Gradient) var gradient
 func _on_health_updated(new_health):
 	$Tween.interpolate_property(
 		self, "value",
-		self.value, new_health, 3,
-		Tween.TRANS_LINEAR, Tween.EASE_OUT
+		self.value, new_health, 2,
+		Tween.TRANS_SINE, Tween.EASE_IN_OUT
 	)
 	$Tween.start()
-	tint_progress = self.gradient.interpolate(self.ratio)
+	
+
+func _on_value_changed(value):
+	self.tint_progress = self.gradient.interpolate(self.ratio)
