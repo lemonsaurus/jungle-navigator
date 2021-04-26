4fcea5bba72121e1a3d8cb7a6bf59592d93d00a9
Leon Sandøy <leon.sandoy@conmodo.com>
2021-04-27 01:20:23 +0200

    add the win-screen!

---
+++ win.gd
@@ -0,0 +1,14 @@
+extends Node
+
+
+func _ready():
+	var dialog = Dialogic.start("TempleRoom")
+	add_child(dialog)
+	dialog.connect("dialogic_signal", self, '_handle_dialogic_event')
+	
+func _handle_dialogic_event(signal_):
+	"""Handle a signal created by Dialogic"""
+	if signal_ == "retry":
+		get_tree().change_scene("res://main.tscn")
+	elif signal_ == "quit":
+		get_tree().quit(0)
